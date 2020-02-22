329858129374201b86e3bf8fa5d352124cb78f2a
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 16:00:22 -0800

    Add phoneNumbers dictionary within PhoneBook class

---
+++ PhoneBook.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Colletions.Generic;
+
+class PhoneBook
+{
+  public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+  static void Main()
+  {
+    //code
+  }
+}
