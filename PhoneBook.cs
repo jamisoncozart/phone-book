ec71b5fa5d34e1b80b031dbf3c8d9acb3983c2e0
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 16:13:49 -0800

    update main() method with phonebook loop with conditionals to add new contact and look up number in phone book

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -1,12 +1,40 @@
 using System;
-using System.Colletions.Generic;
+using System.Collections.Generic;
 
 class PhoneBook
 {
   public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
   static void Main()
   {
-    //code
+    Console.WriteLine("MAIN MENU");
+    Console.WriteLine("Would you like to add a person to your phone book? ['Y' for Yes, 'Enter' for No]");
+    string answer = Console.ReadLine();
+
+    if(answer == "Y" || answer == "y")
+    {
+      AddNewContact();
+    }
+    else
+    {
+      Console.WriteLine("Would you like to look up a number in your phone book? ['Y' for Yes, 'Enter' for No]");
+      string lookUpAnswer = Console.ReadLine();
+      if(lookUpAnswer == "Y" || lookUpAnswer == "y")
+      {
+        LookUpContact();
+      }
+      else
+      {
+        Console.WriteLine("Are you finished using the program? ['Y' for Yes, 'Enter' for No]");
+        string finishedAnswer = Console.ReadLine();
+        if(finishedAnswer == "Y" || finishedAnswer == "y")
+        {
+          Console.WriteLine("Goodbye");
+        }
+        else{
+          Main();
+        }
+      }
+    }
   }
   static void AddNewContact()
   {
@@ -14,6 +42,6 @@ class PhoneBook
   }
   static void LookUpContact()
   {
-    
+
   }
 }
